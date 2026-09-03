e7ecd68d24c21dbfa550bec8ff475d02695dbd8a
salmandaekh-bit <salmandaekh@gmail.com>
2026-09-03 08:53:18 -0400

    9/3
    fixed the index to i

---
+++ JS/javascrtipt.js
@@ -0,0 +1,12 @@
+function welcomeFunction() {
+  var name = prompt("Welcome to my Developer Engineering Notebook! , What's your name?")
+  console.log(name);
+  alert(" Hello ! " + name + "! Ready to learn about my work? ");
+}
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+document.getElementById("date").innerHTML = "<h2>Hello World</h2>";
