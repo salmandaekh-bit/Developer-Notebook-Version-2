d194466397b7bbfb8f5342ff9521b77e3929962f
salmandaekh-bit <salmandaekh@gmail.com>
2026-09-10 16:03:35 -0400

    Weekly Journal Entry
    Weekly Journal Entry

--- JS/javascrtipt.js
+++ JS/javascrtipt.js
@@ -1,12 +1,8 @@
 function welcomeFunction() {
-  var name = prompt("Welcome to my Developer Engineering Notebook! , What's your name?")
-  console.log(name);
-  alert(" Hello ! " + name + "! Ready to learn about my work? ");
+  let project =document.getElementById("welcome");
+  const name = prompt("Welcome to my Developer Engineering Notebook! What's your name?");
+  alert(`Hello! ${name}! Ready to learn about my work?`);
 }
 
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
+welcomeFunction()
 
-document.getElementById("date").innerHTML = "<h2>Hello World</h2>";
